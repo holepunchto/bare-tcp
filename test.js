445769dd5d11ef868633b0651289c40659e97e98
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-04-19 10:49:42 +0200

    Assert that server connection closes

--- test.js
+++ test.js
@@ -5,12 +5,13 @@ test('server + client', async (t) => {
   t.plan(2)
 
   const lc = t.test('lifecycle')
-  lc.plan(1)
+  lc.plan(3)
 
   const server = createServer()
     .on('close', () => t.pass('server closed'))
     .on('connection', (socket) => {
       socket
+        .on('close', () => lc.pass('server connection closed'))
         .on('data', (data) => lc.alike(data.toString(), 'hello world'))
         .end()
     })
@@ -18,7 +19,9 @@ test('server + client', async (t) => {
 
   const { port } = server.address()
 
-  createConnection(port).end('hello world')
+  createConnection(port)
+    .on('close', () => lc.pass('client connection closed'))
+    .end('hello world')
 
   await lc
 
