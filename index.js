1dba872bfbadcb5be13fe89ef2dd04ffe81a36f1
yasser <dev@yasser.email>
2024-08-26 14:11:09 -0300

    Unref socket timer (#7)

--- index.js
+++ index.js
@@ -163,7 +163,9 @@ const Socket = exports.Socket = class TCPSocket extends Duplex {
       this._timer = null
     } else {
       if (ontimeout) this.once('timeout', ontimeout)
+
       this._timer = setTimeout(() => this.emit('timeout'), ms)
+      this._timer.unref()
     }
 
     this._timeout = ms
